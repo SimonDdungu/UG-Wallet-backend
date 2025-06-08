54ed78994286465f3921f523810f5e7de516e3b0
SimonDdungu <dev.simonddungu@example.com>
2025-06-09 02:34:22 +0300

    Minor Update

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
 import { Request, Response } from "express"
-
-const express = require("express")
-const bodyParser = require("body-parser")
-
-const TransactionRoutes = require("./routes/transactions")
-
-//const cors = require("cors")
-
 import ConnectDB, { sql } from "./database/connection"
 import ratelimiter from "./middleware/rate_limiter"
+const express = require("express")
+const bodyParser = require("body-parser")
+require("dotenv").config()
+const TransactionRoutes = require("./routes/transactions")
+//const cors = require("cors")
 
 
 const app = express()
+
+const PORT = process.env.PORT
 
 app.use(ratelimiter)
 app.use(bodyParser.json())
@@ -25,8 +24,8 @@ app.get("/", (req: Request, res: Response) => {
 
 
 ConnectDB().then(() => {
-    app.listen(3005, () => {
-        console.log("Port: ", 3005)
+    app.listen(PORT, () => {
+        console.log("Port: ", PORT)
         console.log("server is running")
     })
 }
